d4ea15f9362362a3aebad65385e01ce8a2c2733e
palumbos <danielpalumbo9@gmail.com>
2026-05-07 10:17:25 -0300

    alterações em aula

---
+++ aula13(for)/combincaoAula135/src/combincaoaula135/CombincaoAula135.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package combincaoaula135;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alunolages
+ */
+public class CombincaoAula135 {
+
+    /**
+     * @param args the command line arguments
+     */
+public static Scanner input = new Scanner(System.in);
+
+ 
+    public static void main(String[] args) {
+        int n, p, combinacao;
+        boolean  continuar = true;
+        
+        while(continuar){
+           System.out.print("digite quantos elementos possue o conjunto: ");
+                  n = Integer.parseInt(input.nextLine());
+         
+           System.out.print("digite quantos elementos serao agrupados por combinacao: ");
+                   p = Integer.parseInt(input.nextLine());
+         
+           combinacao = (caucularFatorial(n)/(caucularFatorial((n - p)) * caucularFatorial(p)));
+           
+            System.out.print("digite se deseja continuar(s para continuar e n para nao continuar):  ");
+                  char comando = input.nextLine().charAt(0);
+                  
+                  continuar = !(comando == 'n' || comando == 'N');
+             
+  
+         
+         System.out.println(combinacao);
+        }
+    } 
+    
+public static Integer   caucularFatorial(int n){
+       int resultado =1;
+       
+       for(int i = 1; i<(n+1); i++){
+            resultado = resultado * i;
+        }
+       return resultado;
+   } 
+}

a0eb150e06f45308e2a9ac78c4ae630493ccb08f
palumbos <danielpalumbo9@gmail.com>
2026-06-08 23:15:25 -0300

    terminei o algortmo sort

--- Lista06-VetoresMatrizesStrings/src/desafio01_array_precoordemcrescente/Main.java
+++ Lista06-VetoresMatrizesStrings/src/desafio01_array_precoordemcrescente/Main.java
@@ -1,27 +1,38 @@
-
 package desafio01_array_precoordemcrescente;
 
 import java.util.Scanner;
 
 public class Main {
-    static Scanner input = new Scanner(System.in);
 
-    public static void main(String args[]) {
-        int produtos;
-        
-        System.out.println("digite quantos produtos tem em sua loja: ");
-        produtos = input.nextInt();
-        
-        float[] preco = new float[produtos];
-        float[] crescente = new float[produtos];
-        
-        for(int i = 0; i < 0; i ++){
-            System.out.println("digite o preço do produto"+(i+1));
-            preco[i] = input.nextFloat();  
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite quantos produtos tem em sua loja: ");
+        int produtos = input.nextInt();
+
+        double[] preco = new double[produtos];
+
+        for (int i = 0; i < produtos; i++) {
+            System.out.println("Digite o preço do produto " + (i + 1) + ": ");
+            preco[i] = input.nextDouble();
         }
-        
-        for(int i = 0; i < 0; i ++){
-            
+
+        double aux;
+        for (int i = 0; i < produtos; i++) {
+            for (int j = i + 1; j < produtos; j++) {
+                if (preco[i] > preco[j]) {
+                    aux = preco[i];
+                    preco[i] = preco[j];
+                    preco[j] = aux;
+                }
+            }
         }
+
+        System.out.println("\nPreços ordenados:");
+        for (int i = 0; i < produtos; i++) {
+            System.out.println(preco[i]);
+        }
+
+        input.close(); 
     }
 }

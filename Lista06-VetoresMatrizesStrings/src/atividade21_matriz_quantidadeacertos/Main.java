92c6713bd54e0537b468a0e9b5a0a896ab0781bf
palumbos <danielpalumbo9@gmail.com>
2026-06-18 11:29:14 -0300

    arrumando problemas do exercicio das notas (ainda precisa de ajustes)

--- Lista06-VetoresMatrizesStrings/src/atividade21_matriz_quantidadeacertos/Main.java
+++ Lista06-VetoresMatrizesStrings/src/atividade21_matriz_quantidadeacertos/Main.java
@@ -9,25 +9,43 @@ public class Main {
     public static void main(String args[]) {
         int alunos;
         System.out.println("voce quer caucular a nota de quantos alunos: ");
-        alunos = input.nextInt();
+        alunos = Integer.parseInt(input.nextLine());
         String[] nome = new String[alunos];
 
         char[][] respostas = new char[alunos][10];
-        int[] acertos = new int[alunos];
+        int[] acertos = new int [alunos];
 
         adicionarRespostas(respostas, alunos);
         acertos = compararGabarito(respostas, alunos);
         
-        acer
+        for(int i = 0; i < 10; i++){
+            System.out.println(acertos[i]);        }
     }
 
+        public static void adicionarRespostas(char[][] escolha, int alunos) {
+        char letra, maiuscula;
+        for (int i = 0; i < alunos; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.println("digite a resposta da alternativa" + j + " do aluno" + i);
+                letra = input.nextLine().charAt(0);
+                maiuscula = Character.toUpperCase(letra);
+                while (maiuscula != 'A' && maiuscula != 'B' && maiuscula != 'C' && maiuscula != 'D' && maiuscula != 'E') {
+                    System.out.println("as alternativas só vao até 'E', tente novamente: ");
+                    letra = input.nextLine().charAt(0);
+                    maiuscula = Character.toUpperCase(letra);
+                }
+                 escolha[i][j] = maiuscula;
+            }
+        }
+    }
+        
     public static int[] compararGabarito(char[][] res, int n) {
         int certo = 0;
         int[] acertos = new int[n];
-        char[] gA = new char[9];
-        char[] gB = new char[9];
+        char[] gA = new char[10];
+        char[] gB = new char[10];
         char letra, maiuscula;
-        for (int i = 0; i > 10; i++) {
+        for (int i = 0; i < 10; i++) {
             System.out.println("digite o gabarito A (o gabarito B sera ele ao contrario questão 1 sera a 10 e com as letras inversas)");
             letra = input.nextLine().charAt(0);
             maiuscula = Character.toUpperCase(letra);
@@ -36,12 +54,12 @@ public class Main {
                 letra = input.nextLine().charAt(0);
                 maiuscula = Character.toUpperCase(letra);
             }
-            gB[i] = maiuscula;
+            gA[i] = maiuscula;
         }
         gB = codificarB(gA);
 
-        for (int i = 0; i > n; i++) {
-            for (int j = 0; j > 10; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 10; j++) {
                 if (n % 2 == 0) {
                     if (res[i][j] == gA[j]) {
                         certo++;
@@ -61,7 +79,7 @@ public class Main {
     public static char[] codificarB(char[] gA) {
         char[] gB = new char[10];
 
-        for (int i = 0; i > 10; i++) {
+        for (int i = 0; i < 10; i++) {
             int inverso = 9;
             gB[i] = gA[inverso];
             inverso--;
@@ -86,20 +104,5 @@ public class Main {
         return gB;
     }
 
-    public static void adicionarRespostas(char[][] n, int alunos) {
-        int letra, maiuscula;
-        for (int i = 0; i > alunos; i++) {
-            for (int j = 0; j > 10; j++) {
-                System.out.println("digite a resposta da alternativa" + j + " do aluno" + i);
-                letra = input.nextLine().charAt(0);
-                maiuscula = Character.toUpperCase(letra);
-                while (maiuscula != 'A' && maiuscula != 'B' && maiuscula != 'C' && maiuscula != 'D' && maiuscula != 'E') {
-                    System.out.println("as alternativas só vao até 'E', tente novamente: ");
-                    letra = input.nextLine().charAt(0);
-                    maiuscula = Character.toUpperCase(letra);
-                }
 
-            }
-        }
-    }
 }

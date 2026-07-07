194d5e1f1573b2f4f0b9162f41e6c39799ecd434
palumbos <danielpalumbo9@gmail.com>
2026-07-07 08:18:29 -0300

    prova 3

---
+++ Provas/Prova3/Prova3Ex1/src/prova3ex1/Prova3Ex1.java
@@ -0,0 +1,67 @@
+package prova3ex1;
+
+import java.util.Scanner;
+
+public class Prova3Ex1 {
+
+    static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int quantProg, quantMat;
+
+        System.out.print("Digite quantos Alunos estão Matriculados em Introdução a Programação: ");
+        quantProg = Integer.parseInt(input.nextLine());
+        if (quantProg < 0 || quantProg > 41) {
+            System.out.println("digite um valor entre 1 e 40");
+            quantProg = Integer.parseInt(input.nextLine());
+        }
+        String[] nomesProg = new String[quantProg];
+        definirNomes(nomesProg, quantProg);
+
+        System.out.print("Digite quantos Alunos estão Matriculados em Matematica Discreta: ");
+        quantMat = Integer.parseInt(input.nextLine());
+        if (quantMat < 0 || quantMat > 41) {
+            System.out.println("digite um valor entre 1 e 40");
+            quantMat = Integer.parseInt(input.nextLine());
+        }
+        String[] nomesMat = new String[quantMat];
+        definirNomes(nomesMat, quantMat);
+
+        verificarNomes(nomesProg, nomesMat, quantProg, quantMat);
+    }
+
+    public static void definirNomes(String[] nomes, int quantidade) {
+        for (int i = 0; i < quantidade; i++) {
+            System.out.print("Digite o nome do aluno numero " + (i + 1) + " ");
+            nomes[i] = input.nextLine();
+        }
+    }
+
+    public static void verificarNomes(String[] prog, String[] mat, int n1, int n2) {
+        String[] alunosApenasProg = new String[n1];
+        int soProg = 0;
+        boolean apenasProg = true;
+
+        for (int i = 0; i < n1; i++) {
+            apenasProg = true;
+            for (int j = 0; j < n2; j++) {
+                if (prog[i].equalsIgnoreCase(mat[j])) {
+                    apenasProg = false;
+                }
+            }
+
+            if (apenasProg) {
+                alunosApenasProg[soProg] = prog[i];
+                soProg++;
+            }
+        }
+
+        System.out.println(soProg);
+        if (soProg != 0) {
+            System.out.println("a Lista de alunos que estão cursando  Introdução  Programação e não estao em Matematica Discreta é: ");
+            for (int i = 0; i < soProg; i++) {
+                System.out.println((i + 1) + " aluno " + alunosApenasProg[i]);
+            }
+        }
+    }
+}
---
+++ Provas/Prova3/Prova3Ex2/src/prova3ex2/Prova3Ex2.java
@@ -0,0 +1,98 @@
+package prova3ex2;
+
+import java.util.Scanner;
+
+public class Prova3Ex2 {
+
+    static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int quantAlunos;
+
+        System.out.print("Digite quantos alunos estão matriculados: ");
+        quantAlunos = Integer.parseInt(input.nextLine());
+        int[][] notas = new int[quantAlunos][5];
+        String[] alunos = lerNotas(quantAlunos, notas);
+        float[] mediaIndiv = caucularMedia(notas);
+        float[] mediaTotal = caucularTotal(notas);
+
+        printarResultado(alunos, mediaIndiv, mediaTotal);
+
+    }
+
+    public static String[] lerNotas(int n, int[][] notas) {
+        String[] aluno = new String[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("digite o nome do " + (i + 1) + " aluno: ");
+            aluno[i] = input.nextLine();
+            for (int j = 0; j < 5; j++) {
+                System.out.print("digite a nota da avaliação " + (j + 1) + " desse aluno: ");
+                notas[i][j] = Integer.parseInt(input.nextLine());
+            }
+        }
+        return aluno;
+    }
+
+    public static float[] caucularMedia(int[][] notas) {
+        float[] mediaInd = new float[notas.length];
+        float media;
+        for (int i = 0; i < notas.length; i++) {
+            media = 0;
+            for (int j = 0; j < 5; j++) {
+                media = media + notas[i][j];
+                
+            }
+            media = media / 5f;
+            mediaInd[i] = media;
+        }
+        return mediaInd;
+    }
+
+    public static float[] caucularTotal(int[][] notas) {
+        float[] mediaTotal = new float[5];
+        float media;
+        for (int i = 0; i < 5; i++) {
+            media = 0;
+            for (int j = 0; j < notas.length; j++) {
+                media = media + notas[j][i];
+            }
+            media = (float) media / notas.length;
+            mediaTotal[i] = media;
+        }
+        return mediaTotal;
+    }
+
+    public static void printarResultado(String[] alunos, float[] mediaIndiv, float[] mediaTotal) {
+        int aprovados = 0;
+        float percentual, maior = 0;
+        for (int i = 0; i < alunos.length; i++) {
+            System.out.println("O aluno " + alunos[i] + " teve a media final de: " + mediaIndiv[i]);
+            if (mediaIndiv[i] >= 6) {
+                System.out.println("Pela media ser maior ou igual a 6, o aluno foi APROVADO!!!");
+            } else if (mediaIndiv[i] >= 3) {
+                System.out.println("Pela media dele ser maior ou igual a 3 e menor que 6, o aluno esta em RECUPERAÇÃO!!!");
+            } else {
+                System.out.println("pela nota do aluno ser menor que 3, o Aluno foi REPROVADO!!!");
+            }
+            if (mediaIndiv[i] >= 6) {
+                aprovados++;
+            }
+        }
+        percentual = (int) (aprovados / alunos.length) * 100; //transformei em int para aparecer o percentual so com a parte inteira
+        System.out.println("\n a quantidade de alunos aprovados foi de " + aprovados + " e o percentual de aprovação foi " + percentual);
+        
+        for ( int i = 0; i < 5; i++){
+            System.out.println("a media final da avaliação "+(i+1)+"foi: "+mediaTotal);
+        }
+        
+        for (int i = 0; i < alunos.length; i++) {
+            if(maior > mediaIndiv[i]){
+            maior = mediaIndiv[i];
+            }
+            
+        }
+        System.out.println(" a maior nota foi "+ maior);
+        
+    }
+    
+}
